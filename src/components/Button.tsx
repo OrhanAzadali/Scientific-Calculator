4adb68c9066ab59e661ee38fea1a40caea85d853
OrhanAzadali <orhanazadali@gmail.com>
2026-01-17 05:35:28 +0400

    SC & Button improved, Temperature - first defined

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,24 @@
-import { ReactElement, JSXElementConstructor, ReactEventHandler, ReactHTMLElement } from 'react';
- 
-interface ButtonProps{
-    savePreviousResult: ReactEventHandler;
-    operationHandler: ReactEventHandler;
+interface ButtonProps {
+    disabled?: boolean;
+    savePreviousResult: React.MouseEventHandler<HTMLButtonElement>;
+    operationHandler: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-type ButtonType = ReactElement<ButtonProps, JSXElementConstructor<'button'>>;
+const Button = ({ disabled, savePreviousResult, operationHandler }: ButtonProps) => {
 
-const Button = ({ savePreviousResult, operationHandler}:ButtonProps):ButtonType => {
+    const class1 = 'md:w-full sm:w-fit h-20 border-2 border-double border-b-yellow-950 bg-amber-700 dark:bg-white/10 rounded-2xl font-red-500 hover:border-red-600 hover:bg-amber-100 hover:text-red-600 hover:animate-pulse text-glow text-shadow-glow shadow-glow-lg animate-glow m-1';
 
-const class1 = 'md:w-full sm:w-fit h-20 border-2 border-double border-b-yellow-950 bg-amber-700 dark:bg-white/10 rounded-2xl font-red-500 hover:border-red-600 hover:bg-amber-100 hover:text-red-600 hover:animate-pulse text-glow text-shadow-glow shadow-glow-lg animate-glow m-1';
+    const class2 = 'md:w-full sm:w-fit h-20 border-2 border-double border-b-green-950 bg-blue-700 dark:bg-white/10 rounded-2xl font-red-500 hover:border-red-600 hover:bg-amber-100 hover:text-red-600 text-glow text-shadow-glow shadow-glow-lg m-1 active:bg-red-700 active:text-white active:animate-pulse active:font-bold';
 
-const class2 = 'md:w-full sm:w-fit h-20 border-2 border-double border-b-green-950 bg-blue-700 dark:bg-white/10 rounded-2xl font-red-500 hover:border-red-600 hover:bg-amber-100 hover:text-red-600 text-glow text-shadow-glow shadow-glow-lg m-1 active:bg-red-700 active:text-white active:animate-pulse active:font-bold';
+    const buttonEventHandler = (e: React.MouseEvent<HTMLButtonElement>, { savePreviousResult, operationHandler }: ButtonProps) => {
+        try { if (!(['resetInput', 'deleting', 'resetResult'].includes(operationHandler.name))) { operationHandler(e); savePreviousResult && savePreviousResult(e) } else { operationHandler(e); } } catch (error: any) { alert(error.message) }
+    };
 
-    return(<button type='button'
-            onClick={(e) => { try { if(!(['resetInput', 'deleting', 'resetResult'].includes(operationHandler.name))) {operationHandler(e); savePreviousResult && savePreviousResult(e)} else{ operationHandler(e); }} catch (error: any) { alert(error.message) } }}
-            className={operationHandler.name === 'deleting' ? class2 : class1}>
-                 {`${operationHandler.name[0].toLocaleUpperCase()}${operationHandler.name.slice(1).replace(/(?=[A-Z])/g, ' ')}`}
-              </button>)
-
-    }
+    return (<button type='button'
+        onClick={(e) => buttonEventHandler(e, { savePreviousResult, operationHandler })}
+        className={operationHandler.name === 'deleting' && !disabled ? class2 : class1} disabled={disabled}>
+        {!disabled ? `${operationHandler.name[0].toLocaleUpperCase()}${operationHandler.name.slice(1).replace(/(?=[A-Z])/g, ' ')} ` :
+            "Nothing to Delete!"}
+    </button>)
+}
 export default Button;
--- src/pages/Temperature.tsx
+++ src/pages/Temperature.tsx
@@ -1,167 +1,226 @@
-import React, {
-  useState,
-  useRef,
-  useEffect,
-  ReactEventHandler,
-  RefObject,
-  RefAttributes,
-  JSXElementConstructor,
-  ReactNode,
-  ReactElement,
-} from "react";
-import * as math from 'mathjs';
+import React, { useState, useRef, useEffect } from "react";
+import { evaluate } from "mathjs";
 import { ErrorBoundary } from "react-error-boundary";
-import Button from '../components/Button';
+import Button from "../components/Button";
 import ErrorPage from "./ErrorPage";
-  // 
-  interface Error {
-    name: string;
-    message: string;
-    stack?: string;
+//
+interface Error {
+  name: string;
+  message: string;
+  stack?: string;
 }
 
-  interface FocusEvent {
-    target: {
-      value: string;
-      name: string;
-      type: string;
-      placeholder: string;
-      id: string;
-    };
-  }
+interface FocusEvent {
+  target: {
+    value: string;
+    name: string;
+    type: string;
+    placeholder: string;
+    id: string;
+  };
+}
 
-  interface OperationEvent {
-    preventDefault: ()=> void;
-  }
+interface OperationEvent {
+  preventDefault: () => void;
+}
 
-const Temperature = ():ReactElement => {
-  const inputReg = /^[0-9e%^*+-/.()=\\s]+$/;
-  const inputRef = useRef<HTMLInputElement | null>(null);
+const Temperature = () => {
+  let selectRef = useRef<HTMLSelectElement | null>(null);
+  let selectRef2 = useRef<HTMLSelectElement | null>(null);
+  let inputRef = useRef<HTMLInputElement | null>(null);
+  let inputRef2 = useRef<HTMLInputElement | null>(null);
+  const typeError = new Error(
+    "PLEASE ENTER A VALID VALUE - ONLY ARITHMETICAL EXPRESSIONS ARE ALLOWED!!!",
+  );
 
-  let [result, setResult] = useState(0);
-  let [message, setMessage] = useState({ text: '', type: '' });
+  let [from, setFrom] = useState(0);
+  let [to, setTo] = useState(0);
+  let [fromMeasure, setFromMeasure] = useState("C");
+  let [toMeasure, setToMeasure] = useState("K");
+  // let [disabled, setDisabled] = useState(false);
 
+  let [message, setMessage] = useState({ text: "", type: "" });
 
   // Handler for focus event
-  const handleFocus = (e: FocusEvent): void => {
-    setMessage({ text: 'Consider that only arithmetical expressions allowed', type: e.target.name });
+  const handleSelectFrom: React.ChangeEventHandler<HTMLSelectElement> = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+  ): void => {
+    setFromMeasure(
+      (val: string) =>
+        (val = selectRef.current ? selectRef.current.value : e.target.value),
+    );
+  };
+  const handleSelectTo: React.ChangeEventHandler<HTMLSelectElement> = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+  ): void => {
+    setToMeasure(
+      (val: string) =>
+        (val = selectRef2.current ? selectRef2.current.value : e.target.value),
+    );
   };
 
- // Handler for blur event (optional)
-  const handleBlur = () => {
-    setMessage({ text: 'Please enter the arithmetical expression to calculate', type: '' });
+  const handleFrom: React.ChangeEventHandler<HTMLInputElement> = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    let tempCelcius: number;
+    // Convert input to Celsius first
+    switch (fromMeasure) {
+      case "C":
+        tempCelcius = Number(inputRef2.current && inputRef2.current.value);
+        break;
+      case "F":
+        tempCelcius =
+          ((Number(inputRef2.current && inputRef2.current.value) - 32) * 5) / 9;
+        break;
+      case "K":
+        tempCelcius =
+          Number(inputRef2.current && inputRef2.current.value) - 273.15;
+        break;
+      default:
+        Number(inputRef2.current && inputRef2.current.value);
+        return;
+    }
+    // inputRef.current && (inputRef.current.value = tempInCelsius);
+    setFrom((val: number) => (val = Number(tempCelcius)));
   };
-  
+
+  const handleTo: React.ChangeEventHandler<HTMLInputElement> = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    let convertedTemp: number;
+    // Convert Celsius to target unit
+    switch (toMeasure) {
+      case "C":
+        convertedTemp = Number(inputRef.current && inputRef.current.value);
+        break;
+      case "F":
+        convertedTemp =
+          (Number(inputRef.current && inputRef.current.value) * 9) / 5 + 32;
+        break;
+      case "K":
+        convertedTemp =
+          Number(inputRef.current && inputRef.current.value) + 273.15;
+        break;
+      default:
+        Number(inputRef.current && inputRef.current.value);
+        return;
+    }
+
+    // inputRef.current && (inputRef.current.value = convertedTemp);
+    setTo((val: number) => (val = Number(convertedTemp)));
+  };
+
+  function resetResult(e: OperationEvent): void {
+    e.preventDefault();
+    setFrom(0);
+    setTo(0);
+    setFromMeasure("C");
+    setToMeasure("C");
+  }
 
   useEffect(() => {
-    console.log(inputRef.current && inputRef.current.value)
-    console.log(inputRef.current && typeof inputRef.current.value)
+    console.log(inputRef.current && inputRef.current.value);
+    console.log(inputRef.current && typeof inputRef.current.value);
   }, [inputRef]);
 
-  let val = (v:HTMLInputElement) => {
-   if(v.value.trim() !== '') {return math.evaluate(v.value.trim());}
-   if(!v || v.value.trim() === '' || !v.value ) { 
-    v.value = "0"; 
-    return 0;
-    };
-  }
-   
-  // (!inputRef.current || inputRef.current.value.trim() === '' || !inputRef.current.value ) ? (inputRef.current ? inputRef.current.value = "0" : null): operation(e) 
-  // Convert degrees - (user entered value) - to radians
-  let radians: ()=> number = ()=> inputRef.current ! && (val(inputRef.current) * (Math.PI / 180));
-  // NEVER use direct storing, use callbacks - OTHERWISE YOU SIMPLY STORE THE PREVIOUS REF VALUE AND THEN USE IT INSTEAD OF THE CURRENT REF VALUE WHICH LEAD TO WRONG RESULTS - ONLY CALLBACKS (THIS WAY) OR USE THE VALUE OF THIS VARIABLE DIRECTLY IN EACH FUNCTION BELOW
-
-    // let tempInCelsius;
-
-    // // Convert input to Celsius first
-    // switch (from) {
-    //     case "C": tempInCelsius = temp; break;
-    //     case "F": tempInCelsius = (temp - 32) * 5/9; break;
-    //     case "K": tempInCelsius = temp - 273.15; break;
-    //     default: resultElement.textContent = "Invalid unit."; return;
-    // }
-
-    // let convertedTemp;
-
-    // // Convert Celsius to target unit
-    // switch (to) {
-    //     case "C": convertedTemp = tempInCelsius; break;
-    //     case "F": convertedTemp = (tempInCelsius * 9/5) + 32; break;
-    //     case "K": convertedTemp = tempInCelsius + 273.15; break;
-    //     default: resultElement.textContent = "Invalid unit."; return;
-    // }
-
-
-  const calcOperations = [
-    function calculate(e: OperationEvent):void {
-      e.preventDefault();
-      if (inputRef.current && inputRef.current.value !== null && !inputReg.test(inputRef.current.value.trim())) { throw new Error('PLEASE ENTER A VALID VALUE - ONLY ARITHMETICAL EXPRESSIONS ARE ALLOWED!!!') }
-      setResult((result) => result = inputRef.current && val(inputRef.current));
-    },
-
-    function resetInput(e:OperationEvent):void {
-      e.preventDefault();
-      inputRef.current ? inputRef.current.value = '' : null
-    },
-
-    function resetResult(e:OperationEvent):void {
-      e.preventDefault();
-      setResult(0)
-    },
-
-    function deleting(e:OperationEvent):void {
-      e.preventDefault();
-      if (inputRef.current && inputRef.current.value.length >= 1) inputRef.current.value = inputRef.current.value.slice(0, - 1)
-    },
-  ];
-
   return (
-    <div className="grid h-full overflow-y-visible justify-items-center p-10 bg-linear-to-tr/decreasing from-yellow-500 via-orange-900 to-orange-500 bg-repeat bg-cover">
-    <ErrorBoundary fallback={
-  <ErrorPage/>} >
-        <div className="grid justify-items-center align-middle border-4 border-emerald-500 bg-linear-to-br/decreasing from-orange-950 via-orange-400 to-orange-950 rounded-2xl w-140 p-4 m-5 gap-8">
-          <div className="m-3 text-4xl border-2 border-emerald-500 p-2 rounded-3xl text-shadow-glow shadow-glow-lg">
-            <h1 className="text-cyan-200 text-shadow-glow shadow-glow-lg animate-pulse">Temperature Converter</h1>
+    <div className="h-screen md:w-full sm:w-fit grid overflow-y-visible justify-items-center p-10 bg-linear-to-tr/decreasing from-yellow-500 via-orange-900 to-orange-500 bg-repeat bg-cover">
+      <ErrorBoundary fallback={<ErrorPage />}>
+        <div className="md:w-full flex flex-col justify-center items-center border-4 border-emerald-500 bg-linear-to-br/decreasing from-orange-950 via-orange-400 to-orange-950 rounded-2xl p-4 m-5 gap-8">
+          <div className="m-3 flex justify-center items-center text-4xl border-2 border-emerald-500 p-2 rounded-3xl text-shadow-glow shadow-glow-lg sm:w-130 ">
+            <h1 className="md:w-fit sm:w-100 text-cyan-200 text-shadow-glow shadow-glow-lg animate-pulse">
+              Temperature Converter
+            </h1>
           </div>
-          <div className="h-45 w-130 p-10 rounded-2xl text-3xl font-extrabold bg-amber-300 outline-3 outline-emerald-300 outline-offset-4 outline-solid border-4 border-cyan-200 text-shadow-blue-700" onClick={(e: OperationEvent) => {inputRef.current ? inputRef.current.value = String(result) : null}}>
-            <p className="dark:bg-white/10 w-fit-content m-1 items-center text-glow text-shadow-glow shadow-glow-lg">
-              {result}
-            </p>
-          </div>
- {message && <div className="text-white flex flex-col justify-center items-center text-shadow-glow shadow-glow-lg animate-pulse">
-           {message.type.length > 0 && <div>
-       <div className="text-yellow-300 text-[0.7rem] text-center">
-                  {message?.type} must consist of the integers, floating numbers and arithmetical operators
-                </div>
-         </div>}<div className="text-1xl text-center text-shadow-glow shadow-glow-lg">
-              {message.text}
-          </div>
-         <input pattern={'[0-9e%^*+-/.()=\\s]+'}
-            id='text'
-            name="text"
-            ref={inputRef}
-            type="text"
-            placeholder="Type a number or any arithmetical expression"
-            className='bg-yellow-500 p-5 dark:bg-white/10 w-120 m-1 rounded-2xl text-1xl text-violet-950 border-2 border-blue-100 focus:border-yellow-500 outline-2 outline-offset-1 focus:border-2 focus:placeholder:text-emerald-400 focus:bg-amber-200 focus:outline-hidden placeholder:text-blue-600 placeholder:text-0.5xl  placeholder:text-center'
-            // onChange={(e) => inputRef.current.value = e.target.value}//redundant and insecure!!!
-            onFocus={(e) => handleFocus(e)}
-            onBlur={() => handleBlur()}
-            autoFocus
-          />
-   
-          </div>}
-          <div className="grid grid-flow-cols h-fit grid-cols-3 rounded-2xl gap-2 p-2 w-115 align-middle content-center items-center text-white border-4 border-x-orange-400 border-y-amber-500 border-double text-glow text-shadow-glow shadow-glow-lg animate-glow">
-            {calcOperations.map((operation, index) => {
-              return <Button key={index} operationHandler={operation}/>
-              // FYI: /([A-Z])/g, ' $&'.toLocaleLowerCase() - alternative regexp syntax for the same result
 
-            })}
+          <div className="md: w-full sm:w-fit text-white grid grid-cols-2 justify-center items-center align-middle mx-35 text-shadow-glow shadow-glow-lg gap-10">
+            <div className="flex flex-col justify-center items-center">
+              {message && (
+                <div className="text-1xl text-center text-shadow-glow shadow-glow-lg animate-pulse">
+                  {message.text}
+
+                  {message.type.length > 0 && (
+                    <div className="text-yellow-300 text-[0.7rem] text-center">
+                      {message?.type} must consist of the integers, floating
+                      numbers and arithmetical operators
+                    </div>
+                  )}
+                </div>
+              )}
+
+              <select
+                title="from"
+                ref={selectRef}
+                value={fromMeasure}
+                className="w-full bg-yellow-500 p-5 dark:bg-white/10 m-1 rounded-2xl text-1xl text-violet-950 border-2 border-blue-100 focus:border-yellow-500 outline-2 outline-offset-1 focus:placeholder:text-emerald-400 focus:bg-amber-200 focus:outline-hidden placeholder:text-blue-600 placeholder:text-0.5xl  
+            placeholder:text-center focus:animate-pulse"
+                // onChange={(e) => inputRef.current.value = e.target.value}//redundant and insecure!!!
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  handleSelectFrom(e)
+                }
+                // onSelectCapture={() => handleBlur()}
+                autoFocus
+              >
+                <option id="celsius" value="C">
+                  Celsius
+                </option>
+                <option id="fahrenheit" value="F">
+                  Fahrenheit
+                </option>
+                <option id="kelvin" value="K">
+                  Kelvin
+                </option>
+              </select>
+              <input
+                title="from"
+                className="w-full bg-yellow-500 p-5 dark:bg-white/10 m-1 rounded-2xl text-1xl text-violet-950 border-2 border-blue-100 focus:border-yellow-500 outline-2 outline-offset-1 focus:placeholder:text-emerald-400 focus:bg-amber-200 focus:outline-hidden placeholder:text-blue-600 placeholder:text-0.5xl  
+            placeholder:text-center focus:animate-pulse"
+                onChange={(e) => handleTo(e)}
+                ref={inputRef}
+                value={String(from)}
+              />
+
+              <select
+                title="from"
+                ref={selectRef2}
+                value={toMeasure}
+                className="w-full bg-yellow-500 p-5 dark:bg-white/10 m-1 rounded-2xl text-1xl text-violet-950 border-2 border-blue-100 focus:border-yellow-500 outline-2 outline-offset-1 focus:placeholder:text-emerald-400 focus:bg-amber-200 focus:outline-hidden placeholder:text-blue-600 placeholder:text-0.5xl  
+            placeholder:text-center focus:animate-pulse"
+                // onChange={(e) => inputRef.current.value = e.target.value}//redundant and insecure!!!
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  handleSelectTo(e)
+                }
+                // onSelectCapture={() => handleBlur()}
+                autoFocus
+              >
+                <option id="cels" value="C">
+                  Celsius
+                </option>
+                <option id="fahr" value="F">
+                  Fahrenheit
+                </option>
+                <option id="kelv" value="K">
+                  Kelvin
+                </option>
+              </select>
+              <input
+                title="to"
+                className="w-full bg-yellow-500 p-5 dark:bg-white/10 m-1 rounded-2xl text-1xl text-violet-950 border-2 border-blue-100 focus:border-yellow-500 outline-2 outline-offset-1 focus:placeholder:text-emerald-400 focus:bg-amber-200 focus:outline-hidden placeholder:text-blue-600 placeholder:text-0.5xl  
+            placeholder:text-center focus:animate-pulse"
+                onChange={(e) => handleFrom(e)}
+                ref={inputRef2}
+                value={String(to)}
+              />
+            </div>
+
+            <button type="button" onClick={(e) => resetResult(e)} className="md:w-full sm:w-fit h-20 border-2 border-double border-b-green-950 bg-blue-700 dark:bg-white/10 rounded-2xl font-red-500 hover:border-red-600 hover:bg-amber-100 hover:text-red-600 text-glow text-shadow-glow shadow-glow-lg m-1 active:bg-red-700 active:text-white active:animate-pulse active:font-bold">
+              RESET
+            </button>
           </div>
-        </div></ErrorBoundary>
+        </div>
+      </ErrorBoundary>
     </div>
   );
-}
-// (e: React.MouseEvent<HTMLButtonElement>) => {  }
+};
 
-
-export default Temperature; 
+export default Temperature;
